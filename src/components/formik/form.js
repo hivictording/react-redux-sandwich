2b69d5caccf8a9a11ce5dde778bc9ddd2b14fcea
hivictording <jianboding@outlook.com>
2020-12-05 18:49:46 -0500

    add basic formik form

--- src/components/formik/form.js
+++ src/components/formik/form.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import {useFormik} from 'formik';
 
 function FormikForm() {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            address: ''
+        },
+        onSubmit: values => console.log(values)
+    })
+
     return (
-        <form className="my-5 w-75 mx-auto">
+        <form className="my-5 w-50 mx-auto" onSubmit={formik.handleSubmit}>
             <div className="form-group">
                 <label htmlFor="name">Enter name</label>
-                <input type="text" name="name" className="form-control"/>
+                <input type="text" name="name" className="form-control" onChange={formik.handleChange} value={formik.values.name}/>
             </div>
             <div className="form-group">
                 <label htmlFor="address">Enter address</label>
-                <input type="text" name="address" className="form-control"/>
+                <input type="text" name="address" className="form-control" onChange={formik.handleChange} value={formik.values.address}/>
             </div>
+            <input type="submit" className="form-control"/>
         </form>
     )
 }
