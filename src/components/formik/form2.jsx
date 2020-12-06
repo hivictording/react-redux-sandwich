cdd6e2d5167f05535e593b2da4a420295ae0ca6b
hivictording <jianboding@outlook.com>
2020-12-06 16:49:21 -0500

    add Field component props implementations

--- src/components/formik/form2.jsx
+++ src/components/formik/form2.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import {Formik,Form,Field,ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 
 // using Formik,Form,Field,ErrorMessage Component
+// using yup for validation
 
 export default function FormikForm() {
     const initValues = {
         name: '',
-        address: ''
+        email: '',
+        address: '',
+        createdOn: '',
+        comments: ''
     }
     const validate = values => {
         let errors = {}
@@ -15,19 +20,28 @@ export default function FormikForm() {
             errors.name = 'name required'
         }
 
-        if (!values.address.trim()) {
-            errors.address = 'address required'
+        if (!values.email.trim()) {
+            errors.email = 'address required'
         }
 
         return errors
     }
+
+    const validationSchema = Yup.object({
+        name: Yup.string().trim().required('name required').min(3,'min lenth: 3').max(5,"max length: 5"),
+        email: Yup.string().trim().email('invalid email format').required('name required'),
+        address: Yup.string().required('address required').min(3,'min length 3'),
+        createdOn: Yup.date().default(() => new Date()),
+        comments: Yup.string()
+    })
 
     const handleSubmit = values => {
         console.log(values)
     }
 
     return (
-        <Formik initialValues={initValues} validate={validate} onSubmit={handleSubmit}>
+        <Formik initialValues={initValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        {/* <Formik initialValues={initValues} validate={validate} onSubmit={handleSubmit}> */}
             <Form className="my-3 mx-auto w-50">
                 <div className="form-group">
                     <label htmlFor="name">Enter Name</label>
@@ -35,10 +49,42 @@ export default function FormikForm() {
                     <ErrorMessage name="name" className="text-warning"/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="address">Enter Address</label>
-                    <Field name="address" className="form-control"/>
-                    <ErrorMessage name="address" className="text-primary"/>
+                    <label htmlFor="email">Enter Email</label>
+                    <Field name="email" type="email" className="form-control"/>
+                    <ErrorMessage name="email" className="text-primary"/>
                 </div>
+
+                {/* Field component props */}
+                <div className="form-group">
+                    <label htmlFor="address">Enter Address</label>
+                    <Field name="address">
+                        {
+                            (props) => {
+                                const {field,form,meta} = props;
+                                console.log(props);
+                                return (
+                                    <div>
+                                        <input className="form-control" {...field}/>
+                                        {meta.touched && meta.error && meta.error}
+                                    </div>
+                                )
+                            }
+                        }
+                    </Field>
+                    {/* <ErrorMessage name="address" className="text-primary"/> */}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="createdOn">Created On</label>
+                    <Field name="createdOn" type="date" className="form-control"/>
+                    <ErrorMessage name="createdOn" className="text-primary"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="comments">Comments</label>
+                    <Field as="textarea" name="comments" className="form-control"/>
+                    <ErrorMessage name="comments"/>
+                </div>
+
+                <input type="submit" className="form-control"/>
             </Form>
         </Formik>
     )
