38ff8aa0550c03453eb3bcb976856263529a8537
hivictording <jianboding@outlook.com>
2020-12-08 19:24:13 -0500

    more formik features test

--- src/components/formik/form4.jsx
+++ src/components/formik/form4.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Formik,Form,Field,ErrorMessage,FieldArray} from 'formik'
 import * as Yup from 'yup'
-
+import './form4.css'
 import Error from './error'
 
 //  using FieldArray with saveValues() function
@@ -11,6 +11,11 @@ const initValues = {
     // tickets: []
     numberOfTickets: 1,
     tickets: [{name:'',email:''}]
+}
+
+const savedValues = {
+    numberOfTickets: 1,
+    tickets: [{name:'vding',email:'aaa@test.com'}]
 }
 
 const validationSchema = Yup.object().shape({
@@ -23,50 +28,69 @@ const validationSchema = Yup.object().shape({
     )
 })
 
-const handleSubmit = values => console.log(values)
+
+const onChangeHandler = (event,values,setValues) => {
+        const originalNumber = values.tickets.length;
+        const originalTickets = [...values.tickets];
+        const currentNumber = event.target.value;
+        let newTicketsArray
+        let currentTickets
+    
+        if (!event.target.value) {
+            setValues({...values,numberOfTickets:event.target.value})
+            return
+        }
+
+        if (currentNumber > originalNumber) {
+            const diff = currentNumber - originalNumber;
+            newTicketsArray = [...new Array(diff)].map((item) => {
+                return {name:'',email:''}
+            })
+            currentTickets = [...originalTickets,...newTicketsArray]
+        } else if (currentNumber <= originalNumber) {
+            newTicketsArray = originalTickets.filter((item,index) => {
+                return index <= currentNumber - 1
+            })
+            currentTickets = [...newTicketsArray]
+        }
+
+        setValues({...values,
+            numberOfTickets:currentNumber,
+            tickets:currentTickets})
+}
+
+// setSubmitting to false after submit
+const handleSubmit = (values,onSubmitProps) => {
+    console.log(values,onSubmitProps)
+    onSubmitProps.setSubmitting(false)
+    onSubmitProps.resetForm()
+}
 
 export default () => {
+    const [savedForms, setSavedForms] = React.useState(null)
+
     return (
-        <Formik initialValues={initValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-            {/* {({ values, setValues }) => ( */}
+        <Formik initialValues={savedForms || initValues} 
+                validationSchema={validationSchema} 
+                onSubmit={handleSubmit}
+                validateOnChange={false}
+                // validateOnBlur={false}
+                validateOnMount
+                enableReinitialize
+                >
+            {(formik) => {
+                // console.log(formik)
+                return (
             <Form className="w-75 mx-auto my-3">
                         <div className="form-group">
-                            <label htmlFor="numberOfTickets">Number of Tickets</label>
+                            <label htmlFor="numberOfTickets">How many tickets do you want to buy?</label>
                             <Field name="numberOfTickets">
                                 {(props) => {
                                     const {form,field} = props;
                                     const {values,setValues} = form;
                                     
                                     const handleChange = (event) => {
-                                        const originalNumber = values.tickets.length;
-                                        const originalTickets = [...values.tickets];
-                                        const currentNumber = event.target.value;
-                                        let newTicketsArray
-                                        let currentTickets
-                                    
-                                        if (!event.target.value) {
-                                            setValues({...values,numberOfTickets:event.target.value})
-                                            return
-                                        }
-
-                                        if (currentNumber > originalNumber) {
-                                            const diff = currentNumber - originalNumber;
-                                            newTicketsArray = [...new Array(diff)].map((item) => {
-                                                return {name:'',email:''}
-                                            })
-                                            currentTickets = [...originalTickets,...newTicketsArray]
-                                        } else if (currentNumber <= originalNumber) {
-                                            newTicketsArray = originalTickets.filter((item,index) => {
-                                                return index <= currentNumber - 1
-                                            })
-                                            currentTickets = [...newTicketsArray]
-                                        }
-
-                                        setValues({...values,
-                                            numberOfTickets:currentNumber,
-                                            tickets:currentTickets})
-
-                                        // field.onChange(event)
+                                        onChangeHandler(event,values,setValues)
                                     }
                                     
                                     return (
@@ -79,11 +103,13 @@ export default () => {
                         <FieldArray name="tickets">
                             {
                                 props => {
-                                    const {form:{values:{tickets}}} = props;
+                                    const {form} = props;
+                                    const {values:{tickets}} = form;
+
                                     return tickets.map((ticket,index) => {
                                         return (
-                                            <div key={index}>
-                                                <div className="form-group">
+                                            <div key={index} className="row align-items-center">
+                                                <div className="form-group col-3">
                                                     <label htmlFor={`name${index}`}>Enter Name</label>
                                                     <Field id={`name${index}`} name={`tickets[${index}].name`} className="form-control"/>
                                                     <ErrorMessage name={`tickets[${index}].name`}>
@@ -92,20 +118,43 @@ export default () => {
                                                         }}
                                                     </ErrorMessage>
                                                 </div>
-                                                <div className="form-group">
+                                                <div className="form-group col-3">
                                                     <label htmlFor={`email${index}`}>Enter Email</label>
                                                     <Field id={`email${index}`} name={`tickets[${index}].email`} className="form-control"/>
                                                     <ErrorMessage name={`tickets[${index}].email`} component={Error}/>
                                                 </div>
+                                                <button type="button" className="btn btn-info form-control col-2 mr-2" onClick={() => {
+                                                    // console.log(`checking name${index}`)
+                                                    // setFieldTouched doesn't work well with nested objects
+                                                    form.setFieldTouched(`tickets[${index}].name`);
+                                                    form.validateField(`tickets[${index}].name`);
+                                                }}>check-name</button>
+                                                <button type="button" className="btn btn-info form-control col-2" onClick={() => {
+                                                    // console.log(`checking email${index}`)
+                                                    
+                                                    form.setFieldTouched(`tickets[${index}].email`);
+                                                    form.validateField(`tickets[${index}].email`);
+                                                }}>check-email</button>
+                                                {/* <button type="button" className="btn-info form-control col-2" onClick={() => {
+                                                    console.log(`visiting both${index}`)
+                                                    
+                                                    form.setTouched({
+                                                        tickets:{}: true,
+                                                        // tickets[index].email: true
+                                                    })
+
+                                                }}>visit-both</button> */}
                                             </div>
                                         )
                                     })
                                 }
                             }
                         </FieldArray>
-                        <input type="submit" className="form-control"/>
+                        <button type="button" className="btn btn-warning mb-2" onClick={() => setSavedForms(savedValues)}>Load Saved Data</button>
+                        <button type="reset" className="btn btn-warning ml-2 mb-2" >Reset Form</button>
+                        <input type="submit" className="form-control" disabled={!formik.isValid || formik.isSubmitting}/>
                     </Form>
-            {/* )} */}
+            )}}
         </Formik>
     )
 }
