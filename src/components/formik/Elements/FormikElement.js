cd2b9bbce17eef7c058cb29ae599d4c68ebcf75f
hivictording <jianboding@outlook.com>
2020-12-10 09:44:32 -0500

    add formik textarea component

--- src/components/formik/Elements/FormikElement.js
+++ src/components/formik/Elements/FormikElement.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Input from './Input'
 import Checkbox from './Checkbox'
+import Textarea from './Textarea'
 
-function FormikElement({control,...rest}) {
-    switch (control) {
+function FormikElement({type,...rest}) {
+    switch (type) {
         case "input":
             return <Input {...rest}/>
+        case "textarea":
+            return <Textarea {...rest}/>
         case "checkbox":
             return <Checkbox {...rest}/>
         default:
--- src/components/formik/form6.jsx
+++ src/components/formik/form6.jsx
@@ -6,12 +6,14 @@ import FormikElement from './Elements/FormikElement'
 
 const initValues = {
     name: '',
-    hobbies: []
+    hobbies: [],
+    comments: ''
 }
 const handleSubmit = values => console.log("values",values)
 const validationSchema = Yup.object({
     name: Yup.string().required('required'),
-    hobbies: Yup.array().of(Yup.string()).min(1,'required')
+    hobbies: Yup.array().of(Yup.string()).min(1,'required'),
+    comments: Yup.string().required('required')
 })
 
 const hobbies = [
@@ -29,8 +31,9 @@ function form() {
                 formik => {
                     return (
                         <Form className="w-75 mx-auto my-3">
-                            <FormikElement control="input" name="name" label="Enter Name"/>
-                            <FormikElement control="checkbox" name="hobbies" label="Your Hobbies" options={hobbies}/>
+                            <FormikElement type="input" name="name" label="Enter Name"/>
+                            <FormikElement type="checkbox" name="hobbies" label="Your Hobbies" options={hobbies}/>
+                            <FormikElement type="textarea" name="comments" label="Comments"/>
                             <button type="submit" className="btn btn-info">Submit</button>
                         </Form>
                     )
